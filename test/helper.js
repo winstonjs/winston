3dd8038704a58b976648e3c37bc47f77ecc62e2b
Matthew Bergman <mzbphoto@gmail.com>
2011-01-02 13:55:15 -0500

    logger tests now working

---
+++ test/helper.js
@@ -0,0 +1,67 @@
+/*
+ * helpers.js: Test helpers for winston
+ *
+ * (C) 2010 Charlie Robbins
+ * MIT LICENSE
+ *
+ */
+
+require.paths.unshift(require('path').join(__dirname, '..', 'lib'));
+ 
+var fs = require('fs'),
+    util = require('util'),
+    path = require('path'),
+    vows = require('vows'),
+    assert = require('assert'),
+    winston = require('winston');
+    loggly = require('loggly');
+
+var helpers = exports;
+
+helpers.loadConfig = function () {
+  try {
+    var configFile = path.join(__dirname, 'data', 'test-config.json'),
+        stats = fs.statSync(configFile)
+        config = JSON.parse(fs.readFileSync(configFile).toString());
+    if (config.subdomain === 'test-subdomain' 
+        || config.auth.username === 'test-username'
+        || config.auth.password === 'test-password') {
+      util.puts('Config file test-config.json must be updated with valid data before running tests');
+      process.exit(0);
+    }
+
+    helpers.config = config;
+    return config;
+  }
+  catch (ex) {
+    util.puts('Config file test-config.json must be created with valid data before running tests');
+    process.exit(0);
+  }
+};
+
+helpers.size = function(obj) {
+    var size = 0, key;
+    for (key in obj) {
+        if (obj.hasOwnProperty(key)) size++;
+    }
+    return size;
+};
+
+
+helpers.assertLogger = function (logger) {
+  assert.instanceOf(logger, winston.Logger);
+  assert.isFunction(logger.log);
+  assert.isFunction(logger.add);
+  assert.isFunction(logger.remove);
+  assert.equal(logger.level, 2);
+};
+
+helpers.assertConsole = function (transport) {
+  assert.instanceOf(transport, winston.Transports.Console);
+  assert.isFunction(transport.log);
+};
+
+helpers.assertRiak = function (transport) {
+  assert.instanceOf(transport, winston.Transports.Riak);
+  assert.isFunction(transport.log);
+};
---
+++ test/logger-test.js
@@ -0,0 +1,120 @@
+/*
+ * interns-test.js: Tests for Loggly interns utility module
+ *
+ * (C) 2010 Charlie Robbins
+ * MIT LICENSE
+ *
+ */
+ 
+var path = require('path'),
+    vows = require('vows'),
+    assert = require('assert');
+    helpers = require('./helper');
+    
+    
+require.paths.unshift(path.join(__dirname, '..', 'lib'));
+
+var winston = require('winston');
+
+vows.describe('winton/logger').addBatch({
+  "The winston logger": {
+    "the logger() method": {
+      topic: function () {
+       return new (winston.Logger)({transports: {"Console": {level: 2}}});
+      },
+      "should return a winston Logger": function (logger) {
+        helpers.assertLogger(logger);
+      }
+    }
+  }
+}).addBatch({
+  "The winston logger": {
+    topic: new (winston.Logger)({transports: {}}),
+    "The logger should start with 0 transports": {
+      topic: function(logger) {
+        return logger;
+      }
+    }, 
+    "should return 0": function(logger) {
+      assert.equal(helpers.size(logger.transports), 0);
+    },
+    "the add() with an unsupported transport": {
+      topic: function (logger) {
+       try {
+         return logger.add("unsupported", {});
+        }
+       catch(err) {
+        return err; 
+       }
+      },
+      "should return an Error": function (err) {
+       assert.instanceOf(err, Error);
+      }
+    },
+    "the add() method with a supported method": {
+      topic: function (logger) {       
+         logger.add("Console", {});  
+         return logger;
+      },
+      "should push a Console Transport onto transports": function (logger) {
+        assert.equal(helpers.size(logger.transports), 1);
+        helpers.assertConsole(logger.transports.Console);
+      },
+      "and adding an additional method": {
+        topic: function (logger) {       
+           logger.add("Riak", {}); 
+           return logger;
+        },
+        "should be able to add multiple transports": function (logger) {
+          assert.equal(helpers.size(logger.transports), 2);
+          helpers.assertConsole(logger.transports.Console);
+          helpers.assertRiak(logger.transports.Riak);
+        }
+      }
+    }
+  }
+}).addBatch({
+  "The winston logger": {
+    topic: new (winston.Logger)({transports: {Console: {}, Riak: {}}}),
+    "The logger should start with 2 transports": {
+      topic: function(logger) {
+        return logger;
+      }
+    }, 
+    "should return 2": function(logger) {
+      assert.equal(helpers.size(logger.transports), 2);
+    },
+    "the remove() with an unadded transport": {
+      topic: function (logger) {
+       try {
+         return logger.remove("Loggly");
+        }
+       catch(err) {
+        return err; 
+       }
+      },
+      "should return an Error": function (err) {
+       assert.instanceOf(err, Error);
+      }
+    },
+    "the remove() method with an added transport": {
+      topic: function (logger) {
+         logger.remove("Console");  
+         return logger;
+      },
+      "should remove a Console Transport from transports": function (logger) {
+        assert.equal(helpers.size(logger.transports), 1);
+        helpers.assertRiak(logger.transports.Riak);
+      },
+      "and removing an additional transport": {
+        topic: function (logger) {
+           logger.remove("Riak");  
+           return logger;
+        },
+        "should remove Riak Transport": function (logger) {
+          assert.equal(helpers.size(logger.transports), 0);
+        }
+      }
+    }
+  }
+}).export(module);
