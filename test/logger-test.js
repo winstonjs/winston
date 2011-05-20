16cf13f539c0383be39b79909d39defa8d677616
indexzero <charlie.robbins@gmail.com>
2011-05-19 22:31:47 -0400

    [fix] Ensure that levelLength is set in the default logger. Check for callbacks before returning errors in `winston.logger.log`

--- test/logger-test.js
+++ test/logger-test.js
@@ -28,7 +28,7 @@ vows.describe('winton/logger').addBatch({
   }
 }).addBatch({
   "An instance of winston.Logger with no transports": {
-    topic: new (winston.Logger)(),
+    topic: new (winston.Logger)({ emitErrs: true }),
     "the log() method should throw an error": function (logger) {
       assert.throws(function () { logger.log('anything') }, Error);
     },
