01061c10a82520fe3be592fb0a3360633ebcadd1
Friedel Ziegelmayer <friedel.ziegelmayer@gmail.com>
2012-10-20 11:16:31 +0200

    [feature] implement a clean way to use util.format in strings; including tests

--- test/logger-test.js
+++ test/logger-test.js
@@ -9,6 +9,7 @@
 var path = require('path'),
     vows = require('vows'),
     assert = require('assert'),
+    util = require('util'),
     winston = require('../lib/winston'),
     helpers = require('./helpers'),
     transport = require('./transports/transport');
@@ -210,6 +211,97 @@ vows.describe('winton/logger').addBatch({
         assert.equal(helpers.size(logger.exceptionHandlers), 2);
         logger.unhandleExceptions();
         assert.equal(helpers.size(logger.exceptionHandlers), 0);
+      }
+    }
+  }
+}).addBatch({
+  "The winston logger": {
+    topic: new (winston.Logger)({
+      transports: [
+        new (winston.transports.Console)()
+      ]
+    }),
+    "the log() method": {
+      "when passed a string placeholder": {
+        topic: function (logger) {
+          logger.once('logging', this.callback);
+          logger.log('info', 'test message %s', 'my string');
+        },
+        "should interpolate": function (transport, level, msg, meta) {
+          assert.strictEqual(msg, 'test message my string');
+        },
+      },
+      "when passed a number placeholder": {
+        topic: function (logger) {
+          logger.once('logging', this.callback);
+          logger.log('info', 'test message %d', 123);
+        },
+        "should interpolate": function (transport, level, msg, meta) {
+          assert.strictEqual(msg, 'test message 123');
+        },
+      },
+      "when passed a json placholder and an empty object": {
+        topic: function (logger) {
+          logger.once('logging', this.callback);
+          logger.log('info', 'test message %j', {number: 123}, {});
+        },
+        "should interpolate": function (transport, level, msg, meta) {
+          assert.strictEqual(msg, 'test message {"number":123}');
+        },
+      },
+      "when passed a escaped percent sign": {
+        topic: function (logger) {
+          logger.once('logging', this.callback);
+          logger.log('info', 'test message %%', {number: 123});
+        },
+        "should not interpolate": function (transport, level, msg, meta) {
+          assert.strictEqual(msg, util.format('test message %%'));
+          assert.deepEqual(meta, {number: 123});          
+        },
+      },
+      "when passed interpolation strings and a meta object": {
+        topic: function (logger) {
+          logger.once('logging', this.callback);
+          logger.log('info', 'test message %s, %s', 'first', 'second' ,{number: 123});
+        },
+        "should interpolate and have a meta object": function (transport, level, msg, meta) {
+          assert.strictEqual(msg, 'test message first, second');
+          assert.deepEqual(meta, {number: 123});
+        },
+      },
+      "when passed multiple strings and a meta object": {
+        topic: function (logger) {
+          logger.once('logging', this.callback);
+          logger.log('info', 'test message', 'first', 'second' , {number: 123});
+        },
+        "should join and have a meta object": function (transport, level, msg, meta) {
+          assert.strictEqual(msg, 'test message first second');
+          assert.deepEqual(meta, {number: 123});
+        },
+      },
+      "when passed interpolations strings, meta object and a callback": {
+        topic: function (logger) {
+          var that = this;
+          logger.log('info', 'test message %s, %s', 'first', 'second' , {number: 123}, function(transport, level, msg, meta){
+            that.callback(transport, level, msg, meta)
+          });
+        },
+        "should interpolate and have a meta object": function (transport, level, msg, meta) {
+          assert.strictEqual(msg, 'test message first, second');
+          assert.deepEqual(meta, {number: 123});
+        },
+      },
+      "when passed multiple strings, a meta object and a callback": {
+        topic: function (logger) {
+          var that = this;
+          logger.log('info', 'test message', 'first', 'second' , {number: 123}, function(transport, level, msg, meta){
+            that.callback(transport, level, msg, meta)
+          });
+        },
+        "should join and have a meta object": function (transport, level, msg, meta) {
+          assert.strictEqual(msg, 'test message first second');
+          assert.deepEqual(meta, {number: 123});
+        },
       }
     }
   }
