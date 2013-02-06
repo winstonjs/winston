c604334c8608e7239d93ea3c6e443f19ec00f7a0
andremrodrigues <ei03019@fe.up.pt>
2013-02-06 15:24:11 +0000

    Adding memory console transport

--- test/helpers.js
+++ test/helpers.js
@@ -79,6 +79,11 @@ helpers.assertConsole = function (transport) {
   assert.isFunction(transport.log);
 };
 
+helpers.assertMemory = function (transport) {
+  assert.instanceOf(transport, winston.transports.Memory);
+  assert.isFunction(transport.log);
+};
+
 helpers.assertFile = function (transport) {
   assert.instanceOf(transport, winston.transports.File);
   assert.isFunction(transport.log);
