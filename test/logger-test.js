fc1c3c7fb7ea05251e017c6c86f9d248d080fa2b
Greg Bergé <neoziro@gmail.com>
2014-04-01 10:18:20 +0200

    Rename "duration" in "durationMs", remove unit.

--- test/logger-test.js
+++ test/logger-test.js
@@ -111,7 +111,6 @@ vows.describe('winton/logger').addBatch({
           "should respond with the appropriate profile message": function (err, level, msg, meta, logger) {
             assert.isNull(err);
             assert.equal(level, 'info');
-            assert.match(meta.duration, /(\d+)ms/);
             assert.isTrue(typeof logger.profilers['test'] === 'undefined');
           },
           "when passed some metadata": {
@@ -130,7 +129,6 @@ vows.describe('winton/logger').addBatch({
               assert.equal(msg, 'test3');
               assert.isNull(err);
               assert.equal(level, 'info');
-              assert.match(meta.duration, /(\d+)ms/);
               assert.equal(meta.some, 'data');
             },
             "when not passed a callback": {
@@ -147,7 +145,6 @@ vows.describe('winton/logger').addBatch({
                 assert.isNull(err);
                 assert.equal(msg, 'test2');
                 assert.equal(level, 'info');
-                assert.match(meta.duration, /(\d+)ms/);
               }
             }
           }
@@ -167,7 +164,6 @@ vows.describe('winton/logger').addBatch({
           "should respond with the appropriate message": function (err, level, msg, meta, logger) {
             assert.isNull(err);
             assert.equal(level, 'info');
-            assert.match(meta.duration, /(\d+)ms/);
           }
         },
         "when not passed a callback": {
@@ -182,11 +178,9 @@ vows.describe('winton/logger').addBatch({
           "should respond with the appropriate message": function (err, transport, level, msg, meta) {
             assert.isNull(err);
             assert.equal(level, 'info');
-            assert.match(meta.duration, /(\d+)ms/);
 
-            var duration = parseInt(meta.duration);
-            assert.isNumber(duration);
-            assert.isTrue(duration >= 50 && duration < 100);
+            assert.isNumber(meta.durationMs);
+            assert.isTrue(meta.durationMs >= 50 && meta.durationMs < 100);
           }
         }
       },
