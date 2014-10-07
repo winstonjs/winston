8e2225799520a4598044cdf93006d216812a27f9
indexzero <charlie.robbins@gmail.com>
2014-10-07 13:14:45 -0400

    [test minor] Unwrap style madness in file-open-test.js and file-stress-test.js

--- test/transports/file-open-test.js
+++ test/transports/file-open-test.js
@@ -6,56 +6,52 @@
  *
  */
 
-!function (assert, fs, os, path, vows, winston) {
-    'use strict';
+var assert = require('assert'),
+    fs = require('fs'),
+    os  = require('os'),
+    path = require('path'),
+    vows = require('vows'),
+    winston = require('../../lib/winston');
 
-    vows.describe('winston/transports/file').addBatch({
-        'An instance of the File Transport': {
-            topic: function () {
-                var callback = this.callback.bind(this),
-                    logPath = path.resolve(__dirname, '../fixtures/logs/file-open-test.log');
+vows.describe('winston/transports/file').addBatch({
+    'An instance of the File Transport': {
+        topic: function () {
+            var callback = this.callback.bind(this),
+                logPath = path.resolve(__dirname, '../fixtures/logs/file-open-test.log');
 
-                try {
-                    fs.unlinkSync(logPath);
-                } catch (ex) {
-                    if (ex && ex.code !== 'ENOENT') { return callback(ex); }
-                }
-
-                var fileTransport = new (winston.transports.File)({
-                        filename: logPath
-                    }),
-                    logger = new (winston.Logger)({
-                        transports: [fileTransport]
-                    }),
-                    timeline = {};
-
-                fileTransport.open(function () {
-                    timeline.open = Date.now();
-
-                    setTimeout(function () {
-                        logger.info('Hello, World!', function () {
-                            timeline.logged = Date.now();
-                        });
-                    }, 100);
-
-                    setTimeout(function () {
-                        callback(null, timeline);
-                    }, 1000);
-                });
-            },
-            'should fire "open" event': function (results) {
-                assert.isTrue(!!results.open);
-            },
-            'should fire "logged" event': function (results) {
-                assert.isTrue(!!results.logged);
+            try {
+                fs.unlinkSync(logPath);
+            } catch (ex) {
+                if (ex && ex.code !== 'ENOENT') { return callback(ex); }
             }
+
+            var fileTransport = new (winston.transports.File)({
+                    filename: logPath
+                }),
+                logger = new (winston.Logger)({
+                    transports: [fileTransport]
+                }),
+                timeline = {};
+
+            fileTransport.open(function () {
+                timeline.open = Date.now();
+
+                setTimeout(function () {
+                    logger.info('Hello, World!', function () {
+                        timeline.logged = Date.now();
+                    });
+                }, 100);
+
+                setTimeout(function () {
+                    callback(null, timeline);
+                }, 1000);
+            });
+        },
+        'should fire "open" event': function (results) {
+            assert.isTrue(!!results.open);
+        },
+        'should fire "logged" event': function (results) {
+            assert.isTrue(!!results.logged);
         }
-    }).export(module);
-}(
-    require('assert'),
-    require('fs'),
-    require('os'),
-    require('path'),
-    require('vows'),
-    require('../../lib/winston')
-);
+    }
+}).export(module);
--- test/transports/file-stress-test.js
+++ test/transports/file-stress-test.js
@@ -6,71 +6,67 @@
  *
  */
 
-!function (assert, fs, os, path, vows, winston) {
-    'use strict';
+var assert = require('assert'),
+    fs = require('fs'),
+    os  = require('os'),
+    path = require('path'),
+    vows = require('vows'),
+    winston = require('../../lib/winston');
 
-    vows.describe('winston/transports/file').addBatch({
-        'A stressed instance of the File Transport': {
-            topic: function () {
-                var callback = this.callback.bind(this),
-                    logPath = path.resolve(__dirname, '../fixtures/logs/file-stress-test.log');
+vows.describe('winston/transports/file').addBatch({
+    'A stressed instance of the File Transport': {
+        topic: function () {
+            var callback = this.callback.bind(this),
+                logPath = path.resolve(__dirname, '../fixtures/logs/file-stress-test.log');
 
-                try {
-                    fs.unlinkSync(logPath);
-                } catch (ex) {
-                    if (ex && ex.code !== 'ENOENT') { return callback(ex); }
-                }
-
-                var fileTransport = new (winston.transports.File)({
-                        filename: logPath
-                    }),
-                    logger = new (winston.Logger)({
-                        transports: [fileTransport]
-                    });
-
-                fileTransport.on('open', function () {
-                    setTimeout(function () {
-                        clearInterval(interval);
-
-                        logger.query({ order: 'asc' }, function (err, results) {
-                            callback(null, results);
-                        });
-                    }, 100);
-                });
-
-                var logIndex = 0,
-                    interval = setInterval(function () {
-                        logger.info(++logIndex);
-                        stress(200);
-                    }, 0);
-
-                logger.info(++logIndex);
-                stress(200);
-
-                function stress(duration) {
-                    var startTime = Date.now();
-
-                    while (Date.now() - startTime < duration) {
-                        Math.sqrt(Math.PI);
-                    }
-                }
-            },
-            'should not skip any log lines': function (results) {
-                var testIndex = 0;
-
-                results.file.forEach(function (log) {
-                    if (+log.message !== ++testIndex) {
-                        throw new Error('Number skipped');
-                    }
-                });
+            try {
+                fs.unlinkSync(logPath);
+            } catch (ex) {
+                if (ex && ex.code !== 'ENOENT') { return callback(ex); }
             }
+
+            var fileTransport = new (winston.transports.File)({
+                    filename: logPath
+                }),
+                logger = new (winston.Logger)({
+                    transports: [fileTransport]
+                });
+
+            fileTransport.on('open', function () {
+                setTimeout(function () {
+                    clearInterval(interval);
+
+                    logger.query({ order: 'asc' }, function (err, results) {
+                        callback(null, results);
+                    });
+                }, 100);
+            });
+
+            var logIndex = 0,
+                interval = setInterval(function () {
+                    logger.info(++logIndex);
+                    stress(200);
+                }, 0);
+
+            logger.info(++logIndex);
+            stress(200);
+
+            function stress(duration) {
+                var startTime = Date.now();
+
+                while (Date.now() - startTime < duration) {
+                    Math.sqrt(Math.PI);
+                }
+            }
+        },
+        'should not skip any log lines': function (results) {
+            var testIndex = 0;
+
+            results.file.forEach(function (log) {
+                if (+log.message !== ++testIndex) {
+                    throw new Error('Number skipped');
+                }
+            });
         }
-    }).export(module);
-}(
-    require('assert'),
-    require('fs'),
-    require('os'),
-    require('path'),
-    require('vows'),
-    require('../../lib/winston')
-);
+    }
+}).export(module);
