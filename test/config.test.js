f4a68fc6cf5dcc14eb2b33a2b94367296aaf8075
indexzero <charlie.robbins@gmail.com>
2015-11-27 18:27:08 -0500

    [refactor dist] Move some files around. Refactor `.addColors()` to be more eager.

---
+++ test/config.test.js
@@ -0,0 +1,15 @@
+/*
+ * config.test.js: Tests for winston.config
+ *
+ * (C) 2010 Charlie Robbins
+ * MIT LICENSE
+ *
+ */
+
+var assume = require('assume'),
+    winston = require('../lib/winston'),
+    helpers = require('./helpers');
+
+describe('winston.config', function () {
+
+});
