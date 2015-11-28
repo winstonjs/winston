3730beedd7651f30dc24845ad9833cbefc2479e8
indexzero <charlie.robbins@gmail.com>
2015-11-28 15:05:30 -0500

    [api] Move `winston.format.default` to `winston.format.simple`. Make `winston.format.json` the STANDARD format.

--- test/formats/format.test.js
+++ test/formats/format.test.js
@@ -14,12 +14,12 @@ describe('winston.format', function () {
     assume(winston.format).is.a('function');
     assume(winston.format.cli).is.a('function');
     assume(winston.format.colorize).is.a('function');
-    assume(winston.format.default).is.a('function');
     assume(winston.format.json).is.a('function');
     assume(winston.format.logstash).is.a('function');
     assume(winston.format.padLevels).is.a('function');
     assume(winston.format.prettyPrint).is.a('function');
     assume(winston.format.splat).is.a('function');
+    assume(winston.format.simple).is.a('function');
     assume(winston.format.uncolorize).is.a('function');
   });
 });
