b3a77f5bbba3d33928739f25371df29b002204c6
indexzero <charlie.robbins@gmail.com>
2017-09-27 00:16:22 -0400

    [doc] Add simple dynamic content example for #1060.

---
+++ examples/dynamic-format-content.js
@@ -0,0 +1,14 @@
+var winston = require('../lib/winston');
+
+var logger = module.exports = winston.createLogger({
+  transports: [new winston.transports.Console()],
+  format: winston.format(
+    winston.format(function dynamicContent(info, opts) {
+      info.message = '[dynamic content] ' + info.message;
+      return info;
+    })(),
+    winston.format.simple()
+  )
+});
+
+logger.log('info', 'This is an information message.');
