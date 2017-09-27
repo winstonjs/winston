f82e475c3d65cae54dc42a6f9e60ab4362b019de
indexzero <charlie.robbins@gmail.com>
2017-09-26 23:34:30 -0400

    [test] More test coverage.

--- test/transports/stream.test.js
+++ test/transports/stream.test.js
@@ -1,14 +1,46 @@
 'use strict';
 
 var path = require('path'),
+    writeable = require('../helpers').writeable,
     winston = require('../../'),
-    helpers = require('../helpers'),
-    fs = require('fs'),
     split = require('split2'),
     assume = require('assume');
 
 describe('Stream({ stream })', function () {
-  it('should support objectMode streams');
-  it('should support UTF8 encoding streams');
-  it('should throw when not passed a stream');
+  it('should support objectMode streams', function (done) {
+    const expected = {
+      level: 'info',
+      message: 'lolwut testing!'
+    };
+
+    const stream = writeable(function (info) {
+      assume(info).equals(expected);
+      done();
+    });
+
+    const transport = new winston.transports.Stream({ stream });
+    transport.log(expected);
+  });
+
+  it('should support UTF8 encoding streams', function (done) {
+    const expected = {
+      level: 'info',
+      message: 'lolwut testing!',
+      raw: 'info: lolwut testing!'
+    };
+
+    const stream = writeable(function (raw) {
+      assume(raw.toString()).equals(expected.raw);
+      done();
+    }, false);
+
+    const transport = new winston.transports.Stream({ stream });
+    transport.log(expected);
+  });
+
+  it('should throw when not passed a stream', function () {
+    assume(function () {
+      const stream = new winston.transports.Stream()
+    }).throws('options.stream is required.');''
+  });
 });
