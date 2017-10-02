4194a49c4ce61412258d60ae991c17ae17f56a4e
indexzero <charlie.robbins@gmail.com>
2017-10-01 22:27:54 -0400

    [fix doc] Update string interpolation example.

--- examples/interpolation.js
+++ examples/interpolation.js
@@ -1,12 +1,28 @@
-var w = require('./');
+const { createLogger, format, transports } = require('../');
+const logger = createLogger({
+  format: format.combine(
+    format.splat(),
+    format.simple()
+  ),
+  transports: [
+    new transports.Console()
+  ]
+});
 
-w.info('Found %s at %s', 'error', new Date());
-w.info('Found %s at %s', 'error', new Error('chill winston'));
-w.info('Found %s at %s', 'error', /WUT/);
-w.info('Found %s at %s', 'error', true);
-w.info('Found %s at %s', 'error', 100.00);
-w.info('Found %s at %s', 'error', ['1, 2, 3']);
+// info: test message my string {}
+logger.log('info', 'test message %s', 'my string');
+
+// info: test message my 123 {}
+logger.log('info', 'test message %d', 123);
+
+// info: test message first second {number: 123}
+logger.log('info', 'test message %s, %s', 'first', 'second', { number: 123 });
+
 // prints "Found error at %s"
+logger.info('Found %s at %s', 'error', new Date());
+logger.info('Found %s at %s', 'error', new Error('chill winston'));
+logger.info('Found %s at %s', 'error', /WUT/);
+logger.info('Found %s at %s', 'error', true);
+logger.info('Found %s at %s', 'error', 100.00);
+logger.info('Found %s at %s', 'error', ['1, 2, 3']);
 
-// console.log('Found %s at %s', 'error', new Date())
-// prints "Found error at Tue Jan 20 2015 10:14:26 GMT-0800 (PST)"
