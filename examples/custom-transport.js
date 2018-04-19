fe9dcc9845a1b35c115c516728ea6bfed0570225
crobbins <crobbins@godaddy.com>
2018-04-19 13:01:34 -0400

    [doc] Add example to illustrate that example code from winston-transport README.md is correct. Related to https://github.com/winstonjs/winston-transport/issues/7.

---
+++ examples/custom-transport.js
@@ -0,0 +1,44 @@
+
+const { createLogger } = require('../');
+const Transport = require('winston-transport');
+const util = require('util');
+
+//
+// Inherit from `winston-transport` so you can take advantage
+// of the base functionality and `.exceptions.handle()`.
+//
+class CustomTransport extends Transport {
+  constructor(opts) {
+    super(opts);
+
+    //
+    // Consume any custom options here. e.g.:
+    // - Connection information for databases
+    // - Authentication information for APIs (e.g. loggly, papertrail,
+    //   logentries, etc.).
+    //
+  }
+
+  log(info, callback) {
+    setImmediate(() => {
+      this.emit('logged', info);
+    });
+
+    // Perform the writing to the remote service
+
+    callback();
+  }
+};
+
+const transport = new CustomTransport();
+transport.on('logged', (info) => {
+  // Verification that log was called on your transport
+  console.log(`Logging! It's happening!`, info);
+});
+
+// Create a logger and consume an instance of your transport
+const logger = createLogger({
+  transports: [transport]
+});
+
+logger.info('hello')
