5223ab9264a9e93603ecbbd87b334ae6d047b698
crobbins <crobbins@godaddy.com>
2018-04-19 17:09:28 -0400

    [doc] Add more copy about printf formats based on @ChrisAlderson's comment in #1175. Fixes #1175.

--- examples/custom-timestamp.js
+++ examples/custom-timestamp.js
@@ -1,15 +1,25 @@
-const winston = require('../');
+const { format, createLogger, transports } = require('../');
 
-const logger = winston.createLogger({
-  format: winston.format.combine(
-    winston.format.label({ label: '[my-label]' }),
-    winston.format.timestamp({
-        format: 'YYYY-MM-DD HH:mm:ss'
+const logger = createLogger({
+  format: format.combine(
+    format.label({ label: '[my-label]' }),
+    format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss'
     }),
-    winston.format.simple()
+    //
+    // The simple format outputs
+    // `${level}: ${message} ${[Object with everything else]}`
+    //
+    format.simple()
+    //
+    // Alternatively you could use this custom printf format if you
+    // want to control where the timestamp comes in your final message.
+    // Try replacing `format.simple()` above with this:
+    //
+    // format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
   ),
   transports: [
-    new winston.transports.Console()
+    new transports.Console()
   ]
 });
 
