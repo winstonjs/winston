2251d0d1f9736c94d6600dc98702b6ba72388e00
DABH <DABH@users.noreply.github.com>
2018-04-21 16:21:53 -0700

    Crazy/good declaration merging, ugh

--- index.d.ts
+++ index.d.ts
@@ -4,183 +4,16 @@
 /// <reference types="node" />
 
 import {Format, FormatWrap} from 'logform';
+import * as logform from 'logform';
+import * as Config from './lib/winston/config/index';
+import * as Transports from './lib/winston/transports/index';
 import * as Transport from 'winston-transport';
 import * as stream from "stream";
-import {Agent} from "http";
 
 declare namespace winston {
-    // TRANSPORTS
-
-    interface ConsoleTransportOptions extends Transport.TransportStreamOptions {
-        stderrLevels?: string[];
-        debugStdout?: boolean;
-        eol?: string;
-    }
-
-    interface ConsoleTransportInstance extends Transport {
-        name: string;
-        stderrLevels: string[];
-        eol: string;
-
-        new(options?: ConsoleTransportOptions): ConsoleTransportInstance;
-    }
-
-    interface FileTransportOptions extends Transport.TransportStreamOptions {
-        filename?: string;
-        dirname?: string;
-        options?: object;
-        maxsize?: number;
-        stream?: NodeJS.WritableStream;
-        rotationFormat?: Function;
-        zippedArchive?: boolean;
-        maxFiles?: number;
-        eol?: string;
-        tailable?: boolean;
-    }
-
-    interface FileTransportInstance extends Transport {
-        name: string;
-        filename: string;
-        dirname: string;
-        options: object;
-        maxsize: number | null;
-        rotationFormat: Function | boolean;
-        zippedArchive: boolean;
-        maxFiles: number | null;
-        eol: string;
-        tailable: boolean;
-
-        new(options?: FileTransportOptions): FileTransportInstance;
-    }
-
-    interface HttpTransportOptions extends Transport.TransportStreamOptions {
-        ssl?: any;
-        host?: string;
-        port?: number;
-        auth?: { username: string; password: string; };
-        path?: string;
-        agent?: Agent;
-        headers?: object;
-    }
-
-    interface HttpTransportInstance extends Transport {
-        name: string;
-        ssl: boolean;
-        host: string;
-        port: number;
-        auth?: { username: string, password: string };
-        path: string;
-        agent?: Agent | null;
-
-        new(options?: HttpTransportOptions): HttpTransportInstance;
-    }
-
-    interface StreamTransportOptions extends Transport.TransportStreamOptions {
-        stream: NodeJS.WritableStream;
-    }
-
-    interface StreamTransportInstance extends Transport {
-        new(options?: StreamTransportOptions): StreamTransportInstance;
-    }
-
-    interface Transports {
-        File: FileTransportInstance;
-        Console: ConsoleTransportInstance;
-        Http: HttpTransportInstance;
-        Stream: StreamTransportInstance;
-    }
-
-    // CONFIG
-
-    interface AbstractConfigSetLevels {
-        [key: string]: number;
-    }
-
-    interface AbstractConfigSetColors {
-        [key: string]: string | string[];
-    }
-
-    interface AbstractConfigSet {
-        levels: AbstractConfigSetLevels;
-        colors: AbstractConfigSetColors;
-    }
-
-    interface CliConfigSetLevels extends AbstractConfigSetLevels {
-        error: number;
-        warn: number;
-        help: number;
-        data: number;
-        info: number;
-        debug: number;
-        prompt: number;
-        verbose: number;
-        input: number;
-        silly: number;
-    }
-
-    interface CliConfigSetColors extends AbstractConfigSetColors {
-        error: string | string[];
-        warn: string | string[];
-        help: string | string[];
-        data: string | string[];
-        info: string | string[];
-        debug: string | string[];
-        prompt: string | string[];
-        verbose: string | string[];
-        input: string | string[];
-        silly: string | string[];
-    }
-
-    interface NpmConfigSetLevels extends AbstractConfigSetLevels {
-        error: number;
-        warn: number;
-        info: number;
-        verbose: number;
-        debug: number;
-        silly: number;
-    }
-
-    interface NpmConfigSetColors extends AbstractConfigSetColors {
-        error: string | string[];
-        warn: string | string[];
-        info: string | string[];
-        verbose: string | string[];
-        debug: string | string[];
-        silly: string | string[];
-    }
-
-    interface SyslogConfigSetLevels extends AbstractConfigSetLevels {
-        emerg: number;
-        alert: number;
-        crit: number;
-        error: number;
-        warning: number;
-        notice: number;
-        info: number;
-        debug: number;
-    }
-
-    interface SyslogConfigSetColors extends AbstractConfigSetColors {
-        emerg: string | string[];
-        alert: string | string[];
-        crit: string | string[];
-        error: string | string[];
-        warning: string | string[];
-        notice: string | string[];
-        info: string | string[];
-        debug: string | string[];
-    }
-
-    interface Config {
-        allColors: AbstractConfigSetColors;
-        cli: { levels: CliConfigSetLevels, colors: CliConfigSetColors };
-        npm: { levels: NpmConfigSetLevels, colors: NpmConfigSetColors };
-        syslog: { levels: SyslogConfigSetLevels, colors: SyslogConfigSetColors };
-
-        addColors(colors: AbstractConfigSetColors): void;
-
-        colorize(level: number, message?: string): string;
-    }
+    export import format = logform.format;
+    export import config = Config;
+    export import transports = Transports;
 
     // MAIN
 
@@ -237,7 +70,7 @@ declare namespace winston {
     }
 
     interface LoggerOptions {
-        levels?: AbstractConfigSetLevels;
+        levels?: Config.AbstractConfigSetLevels;
         silent?: string;
         format?: Format;
         level?: string;
@@ -249,7 +82,7 @@ declare namespace winston {
     interface Logger extends stream.Transform {
         silent: boolean;
         format: Format;
-        levels: AbstractConfigSetLevels;
+        levels: Config.AbstractConfigSetLevels;
         level: string;
         transports: Transport[];
         paddings: string[];
@@ -298,36 +131,32 @@ declare namespace winston {
         options: object;
     }
 
-    interface Winston {
-        version: string;
-        transports: Transports;
-        config: Config;
-        // Transport: Transport;
-        ExceptionHandler: ExceptionHandler;
-        Container: Container;
-        loggers: Container;
+    let version: string;
+    // let transports: Transports;
+    // let config: Config;
+    // Transport: Transport;
+    let ExceptionHandler: ExceptionHandler;
+    let Container: Container;
+    let loggers: Container;
 
-        addColors(target: AbstractConfigSetColors): any;
-        format(formatFn?: Function): Format | FormatWrap;
-        createLogger(options?: LoggerOptions): Logger;
+    let addColors: (target: Config.AbstractConfigSetColors) => any;
+    let createLogger: (options?: LoggerOptions) => Logger;
 
-        // Pass through the target methods to the default logger.
-        log: LogMethod;
-        query(options?: QueryOptions, callback?: (err: Error, results: any) => void): any;
-        stream(options?: any): NodeJS.ReadableStream;
-        add(transport: Transport): Logger;
-        remove(transport: Transport): Logger;
-        clear(): Logger;
-        startTimer(): Profiler;
-        profile(id: string | number): Logger;
-        configure(options: LoggerOptions): void;
-        level: string;
-        exceptions: ExceptionHandler;
-        paddings: string[];
-        exitOnError: Function | boolean;
-        "default": object;
-    }
+    // Pass through the target methods to the default logger.
+    let log: LogMethod;
+    let query: (options?: QueryOptions, callback?: (err: Error, results: any) => void) => any;
+    let stream: (options?: any) => NodeJS.ReadableStream;
+    let add: (transport: Transport) => Logger;
+    let remove: (transport: Transport) => Logger;
+    let clear: () => Logger;
+    let startTimer: () => Profiler;
+    let profile: (id: string | number) => Logger;
+    let configure: (options: LoggerOptions) => void;
+    let level: string;
+    let exceptions: ExceptionHandler;
+    let paddings: string[];
+    let exitOnError: Function | boolean;
+    // let default: object;
 }
 
-declare const winston: winston.Winston;
 export = winston;
