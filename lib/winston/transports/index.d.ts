21a850a1de971fa91523b0f4a55f9b971fa4fd70
DABH <DABH@users.noreply.github.com>
2018-04-22 18:08:43 -0700

    Proper formatting for things; gitignore webstorm .idea folder

--- lib/winston/transports/index.d.ts
+++ lib/winston/transports/index.d.ts
@@ -7,86 +7,84 @@ import * as Transport from 'winston-transport';
 import {Agent} from "http";
 
 declare namespace winston {
+  interface ConsoleTransportOptions extends Transport.TransportStreamOptions {
+    stderrLevels?: string[];
+    debugStdout?: boolean;
+    eol?: string;
+  }
 
-    interface ConsoleTransportOptions extends Transport.TransportStreamOptions {
-        stderrLevels?: string[];
-        debugStdout?: boolean;
-        eol?: string;
-    }
+  interface ConsoleTransportInstance extends Transport {
+    name: string;
+    stderrLevels: string[];
+    eol: string;
 
-    interface ConsoleTransportInstance extends Transport {
-        name: string;
-        stderrLevels: string[];
-        eol: string;
+    new(options?: ConsoleTransportOptions): ConsoleTransportInstance;
+  }
 
-        new(options?: ConsoleTransportOptions): ConsoleTransportInstance;
-    }
+  interface FileTransportOptions extends Transport.TransportStreamOptions {
+    filename?: string;
+    dirname?: string;
+    options?: object;
+    maxsize?: number;
+    stream?: NodeJS.WritableStream;
+    rotationFormat?: Function;
+    zippedArchive?: boolean;
+    maxFiles?: number;
+    eol?: string;
+    tailable?: boolean;
+  }
 
-    interface FileTransportOptions extends Transport.TransportStreamOptions {
-        filename?: string;
-        dirname?: string;
-        options?: object;
-        maxsize?: number;
-        stream?: NodeJS.WritableStream;
-        rotationFormat?: Function;
-        zippedArchive?: boolean;
-        maxFiles?: number;
-        eol?: string;
-        tailable?: boolean;
-    }
+  interface FileTransportInstance extends Transport {
+    name: string;
+    filename: string;
+    dirname: string;
+    options: object;
+    maxsize: number | null;
+    rotationFormat: Function | boolean;
+    zippedArchive: boolean;
+    maxFiles: number | null;
+    eol: string;
+    tailable: boolean;
 
-    interface FileTransportInstance extends Transport {
-        name: string;
-        filename: string;
-        dirname: string;
-        options: object;
-        maxsize: number | null;
-        rotationFormat: Function | boolean;
-        zippedArchive: boolean;
-        maxFiles: number | null;
-        eol: string;
-        tailable: boolean;
+    new(options?: FileTransportOptions): FileTransportInstance;
+  }
 
-        new(options?: FileTransportOptions): FileTransportInstance;
-    }
+  interface HttpTransportOptions extends Transport.TransportStreamOptions {
+    ssl?: any;
+    host?: string;
+    port?: number;
+    auth?: { username: string; password: string; };
+    path?: string;
+    agent?: Agent;
+    headers?: object;
+  }
 
-    interface HttpTransportOptions extends Transport.TransportStreamOptions {
-        ssl?: any;
-        host?: string;
-        port?: number;
-        auth?: { username: string; password: string; };
-        path?: string;
-        agent?: Agent;
-        headers?: object;
-    }
+  interface HttpTransportInstance extends Transport {
+    name: string;
+    ssl: boolean;
+    host: string;
+    port: number;
+    auth?: { username: string, password: string };
+    path: string;
+    agent?: Agent | null;
 
-    interface HttpTransportInstance extends Transport {
-        name: string;
-        ssl: boolean;
-        host: string;
-        port: number;
-        auth?: { username: string, password: string };
-        path: string;
-        agent?: Agent | null;
+    new(options?: HttpTransportOptions): HttpTransportInstance;
+  }
 
-        new(options?: HttpTransportOptions): HttpTransportInstance;
-    }
+  interface StreamTransportOptions extends Transport.TransportStreamOptions {
+    stream: NodeJS.WritableStream;
+  }
 
-    interface StreamTransportOptions extends Transport.TransportStreamOptions {
-        stream: NodeJS.WritableStream;
-    }
+  interface StreamTransportInstance extends Transport {
+    new(options?: StreamTransportOptions): StreamTransportInstance;
+  }
 
-    interface StreamTransportInstance extends Transport {
-        new(options?: StreamTransportOptions): StreamTransportInstance;
-    }
-
-    interface Transports {
-        File: FileTransportInstance;
-        Console: ConsoleTransportInstance;
-        Http: HttpTransportInstance;
-        Stream: StreamTransportInstance;
-    }
-
+  interface Transports {
+    File: FileTransportInstance;
+    Console: ConsoleTransportInstance;
+    Http: HttpTransportInstance;
+    Stream: StreamTransportInstance;
+  }
 }
 
 declare const winston: winston.Transports;
