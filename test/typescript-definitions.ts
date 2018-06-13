1d0a5ac7953016781234fdebcc0b3c75ca61d44a
Chris Alderson <ChrisAlderson@protonmail.com>
2018-06-13 20:27:25 +0200

    [dist] Add container methods to typescript definitions  (#1362)
    * [dist] Add container methods to typescript definitions
    
    * [test] Add tests for container methods for typescript

--- test/typescript-definitions.ts
+++ test/typescript-definitions.ts
@@ -14,7 +14,19 @@ logger.log('info', 'hey dude', { foo: 'bar' });
 logger.log({ level: 'info', message: 'hey dude', meta: { foo: 'bar' } });
 
 // Default logger
-winston.http('New incoming connection')
-winston.error('The error was: ', err)
+winston.http('New incoming connection');
+winston.error('The error was: ', err);
 
 winston.exceptions.handle(new winston.transports.File({ filename: 'exceptions.log' }));
+
+const loggerOptions: winston.LoggerOptions = {
+    level: 'info',
+    format: winston.format.json(),
+    transports: [
+        new winston.transports.Console({ level: 'info' }),
+    ],
+};
+winston.loggers.add('category', loggerOptions);
+winston.loggers.get('category', loggerOptions);
+winston.loggers.has('category');
+winston.loggers.close();
