c910534de56cb2a9b36a36cc08ae4aa53c93ed1c
indexzero <charlie.robbins@gmail.com>
2018-12-23 18:25:52 -0500

    [doc] Document all possible permuations for splat.

--- examples/splat.js
+++ examples/splat.js
@@ -1,23 +1,34 @@
 const winston = require('../');
+let { format } = winston;
 
-let transports = {
-  console: new winston.transports.Console({level: 'info'})
-};
+/*
+ * Simple helper for stringifying all remaining
+ * properties.
+ */
+function rest(info) {
+  return JSON.stringify(Object.assign({}, info, {
+    level: undefined,
+    message: undefined,
+    splat: undefined,
+    label: undefined
+  }));
+}
 
-let {format} = winston;
 let logger = winston.createLogger({
+  transports: [new winston.transports.Console({ level: 'info' })],
   format: format.combine(
     format.splat(),
-    format.simple()
-  ),
-  transports: [transports.console]
+    format.printf(info => `[${info.label}] ${info.message} ${rest(info)}`)
+  )
 });
 
-
 logger.log(
   'info',
   'any message',
-  { label: 'label!' }
+  {
+    label: 'label!',
+    extra: true
+  }
 );
 
 logger.log(
@@ -25,53 +36,51 @@ logger.log(
   'let\'s %s some %s',
   'interpolate',
   'splat parameters',
-  { label: 'label!' }
-);
-
-const terr = new Error('lol please stop doing this');
-terr.enum = true;
-logger.log(
-  'info',
-  'any message',
-  terr
-);
-
-logger.log(
-  'info',
-  'let\'s %s some %s',
-  'interpolate',
-  'splat parameters',
-  terr
+  {
+    label: 'label!',
+    extra: true
+  }
 );
 
 logger.log(
   'info',
   'first is a string %s [[%j]]',
   'behold a string',
-  { lol: 'what did charlie do' }
+  { beAware: 'this will interpolate' },
+  {
+    label: 'label!',
+    extra: true
+  }
 );
 
 logger.log(
   'info',
   'first is an object [[%j]]',
-  { lol: 'what did charlie do' }
+  { beAware: 'this will interpolate' },
+  {
+    label: 'label!',
+    extra: true
+  }
 );
 
-/*
-const simple = winston.createLogger({
-  level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
-  format: winston.format.simple(),
-  transports: [new winston.transports.Console()],
-});
+//
+// Non-enumerable properties (such as "message" and "stack" in Error
+// instances) will not be merged into any `info`.
+//
+const terr = new Error('lol please stop doing this');
+terr.label = 'error';
+terr.extra = true;
+logger.log(
+  'info',
+  'any message',
+  terr
+);
 
-// Implicit Object.assign to message
-const email = {
-  subject: 'Hello, World!',
-  message: 'Hello mr.World, sorry, but you will not see this message in the console.',
-};
+logger.log(
+  'info',
+  'let\'s %s some %s',
+  'interpolate',
+  'splat parameters',
+  terr
+);
 
-console.log('console.log:', email);//works as expected
-simple.info('simple.info: ', email);//no 'message'
-logger.info('logger.info: ', email);//no 'message'
-console.log('console.log again:', email);//still works as expected
-*/
