daeefbf8a929ab1ede9d6de4c0fe2b077fa194d8
Atef Ben Ali <atefBB@users.noreply.github.com>
2019-06-29 22:31:11 +0100

    Fix logs files names (#1651)

--- examples/quick-start.js
+++ examples/quick-start.js
@@ -13,8 +13,8 @@ const logger = createLogger({
   defaultMeta: { service: 'your-service-name' },
   transports: [
     //
-    // - Write to all logs with level `info` and below to `combined.log`
-    // - Write all logs error (and below) to `error.log`.
+    // - Write to all logs with level `info` and below to `quick-start-combined.log`.
+    // - Write all logs error (and below) to `quick-start-error.log`.
     //
     new transports.File({ filename: 'quick-start-error.log', level: 'error' }),
     new transports.File({ filename: 'quick-start-combined.log' })
