44862ff1956ab9ca9f63ad515c8ca43e634289ca
Stefan Aebischer <aebischer@pixtron.ch>
2019-12-02 22:54:41 -0500

    Zip archives if zippedArchive is true
    * Zip archives during rotation if zippedArchive is true
    * Remove gzip stream which was never written to
      (see https://github.com/winstonjs/winston/issues/1128#issuecomment-460270458)
    * ads tests checking the contents of zipped archives
    * ads tests for rolling files with tailable: false
    * Fixes previously failing test tests/tail.file.test.js

--- test/transports/file-archive.test.js
+++ test/transports/file-archive.test.js
@@ -7,6 +7,7 @@
  */
 
 /* eslint-disable no-sync */
+const zlib = require('zlib');
 const assert = require('assert');
 const rimraf = require('rimraf');
 const fs = require('fs');
@@ -24,6 +25,16 @@ function removeFixtures(done) {
 
 
 let archiveTransport = null;
+
+function data(ch, kb) {
+  return String.fromCharCode(65 + ch).repeat(kb * 1024 - 1);
+}
+
+function logKbytes(kbytes, txt) {
+  const toLog = {};
+  toLog[MESSAGE] = data(txt, kbytes);
+  archiveTransport.log(toLog);
+}
 
 describe('winston/transports/file/zippedArchive', function () {
   describe('An instance of the File Transport with tailable true', function () {
@@ -46,16 +57,6 @@ describe('winston/transports/file/zippedArchive', function () {
     it('when created archived files are rolled', function (done) {
       let created = 0;
       let loggedTotal = 0;
-
-      function data(ch, kb) {
-        return String.fromCharCode(65 + ch).repeat(kb * 1024 - 1);
-      }
-
-      function logKbytes(kbytes, txt) {
-        const toLog = {};
-        toLog[MESSAGE] = data(txt, kbytes);
-        archiveTransport.log(toLog);
-      }
 
       archiveTransport.on('logged', function (info) {
         loggedTotal += info[MESSAGE].length + 1;
@@ -87,6 +88,108 @@ describe('winston/transports/file/zippedArchive', function () {
         assert.doesNotThrow(function () {
           fs.statSync(fullpath);
         }, Error);
+      }
+    });
+
+    it('testarchive.log should be ascii, testarchive1.log.gz and testarchive2.log.gz should be zipped', function () {
+      const letters = ['D', 'C', 'B'];
+
+      for (var num = 0; num < 3; num++) {
+        let content;
+        const letter = letters[num];
+        const file = !num ? 'testarchive.log' : 'testarchive' + num + '.log.gz';
+        const fileContent = fs.readFileSync(path.join(__dirname, '..', 'fixtures', 'logs', file));
+
+        if (num > 0) {
+          // archives should be zipped
+          assert.doesNotThrow(function () {
+            content = zlib.gunzipSync(fileContent).toString('ascii');
+          });
+        } else {
+          // current log file should be plain text
+          content = fileContent.toString('ascii');
+        }
+
+        assert(content.match(new RegExp(letter, 'g'))[0].length, content.length);
+      }
+    });
+  });
+
+  describe('An instance of the File Transport with tailable false', function () {
+    before(removeFixtures);
+    after(removeFixtures);
+
+    it('init logger AFTER cleaning up old files', function () {
+      archiveTransport = new winston.transports.File({
+        timestamp: true,
+        json: false,
+        zippedArchive: true,
+        tailable: false,
+        filename: 'testarchive.log',
+        dirname: path.join(__dirname, '..', 'fixtures', 'logs'),
+        maxsize: 4096,
+        maxFiles: 3
+      });
+    });
+
+    it('when created archived files are rolled', function (done) {
+
+      let created = 0;
+      let loggedTotal = 0;
+
+      archiveTransport.on('logged', function (info) {
+        loggedTotal += info[MESSAGE].length + 1;
+        if (loggedTotal >= 14 * 1024) { // just over 3 x 4kb files
+          return done();
+        }
+
+        if (loggedTotal % 4096 === 0) {
+          created++;
+        }
+        // eslint-disable-next-line max-nested-callbacks
+        setTimeout(() => logKbytes(1, created), 1);
+      });
+
+      logKbytes(1, created);
+    });
+
+    it('should be only 3 files called testarchive3.log, testarchive2.log.gz and testarchive1.log.gz', function () {
+      for (var num = 0; num < 4; num++) {
+        const file = num === 3 ? 'testarchive3.log' : 'testarchive' + (num > 0 ? num : '') + '.log.gz';
+        const fullpath = path.join(__dirname, '..', 'fixtures', 'logs', file);
+
+        const statFile = function () {
+          fs.statSync(fullpath);
+        };
+
+        if (num === 0) {
+          assert.throws(statFile, Error);
+        } else {
+          assert.doesNotThrow(statFile, Error);
+        }
+      }
+    });
+
+    it('testarchive3.log should be ascii, testarchive2.log.gz and testarchive1.log.gz should be zipped', function () {
+      const letters = ['B', 'C', 'D'];
+
+      for (var num = 1; num < 4; num++) {
+        let content;
+        const letter = letters[num - 1];
+        const file = num === 3 ? 'testarchive3.log' : 'testarchive' + num + '.log.gz';
+        const fileContent = fs.readFileSync(path.join(__dirname, '..', 'fixtures', 'logs', file));
+
+        if (num !== 3) {
+          // archives should be zipped
+          assert.doesNotThrow(function () {
+            content = zlib.gunzipSync(fileContent).toString('ascii');
+          });
+        } else {
+          // current log file should be plain text
+          content = fileContent.toString('ascii');
+        }
+
+        assert(content.match(new RegExp(letter, 'g'))[0].length, content.length);
       }
     });
   });
