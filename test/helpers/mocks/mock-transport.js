85ea8a610a9b4428c525663ea6e29d9e5fb431fd
Jonathon Terry <10217028+Maverick1872@users.noreply.github.com>
2022-01-30 20:03:15 -0700

    feat: introduce a in-memory mock transport generator

--- test/helpers/mocks/mock-transport.js
+++ test/helpers/mocks/mock-transport.js
@@ -1,9 +1,11 @@
 const stream = require('stream')
 const winston = require('../../../lib/winston');
+const {Writable} = require("stream");
+const {output} = require("./legacy-transport");
 
 /**
  * Returns a new Winston transport instance which will invoke
- * the `write` method on each call to `.log`
+ * the `write` method on each call to `.log`
  *
  * @param {function} write Write function for the specified stream
  * @returns {StreamTransportInstance} A transport instance
@@ -17,6 +19,23 @@ function createMockTransport(write) {
   return new winston.transports.Stream({ stream: writeable })
 }
 
+/**
+ * Returns a valid Winston transport that writes to the passed array object
+ * @param array Array to be used to store the "written" chunks
+ * @returns {winston.transports.Stream}
+ */
+function inMemory(array) {
+  const memoryStream = new Writable({
+    objectMode: true,
+    write: (chunk, encoding, next) => {
+      array.push(chunk);
+      next()
+    }
+  });
+  return new winston.transports.Stream({stream: memoryStream})
+}
+
 module.exports = {
-  createMockTransport
+  createMockTransport,
+  inMemory
 };
