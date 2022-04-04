7937f337dac50b4f134bbfa88f72bdd313ad0af5
wbt <wbt@users.noreply.github.com>
2022-04-04 18:10:48 -0400

    Revert for 2103 (#2104)
    * Revert "fix(metadata precedence) - fix metadata override issues (#1989)"
    
    This reverts commit ba93eaeac36b41456789dd71f7ba5a0342ea5736.
    
    * Update changelog
    
    * Update version number

--- test/helpers/mocks/mock-transport.js
+++ test/helpers/mocks/mock-transport.js
@@ -1,10 +1,9 @@
 const stream = require('stream')
 const winston = require('../../../lib/winston');
-const {Writable} = require("stream");
 
 /**
  * Returns a new Winston transport instance which will invoke
- * the `write` method on each call to `.log`
+ * the `write` method on each call to `.log`
  *
  * @param {function} write Write function for the specified stream
  * @returns {StreamTransportInstance} A transport instance
@@ -18,23 +17,6 @@ function createMockTransport(write) {
   return new winston.transports.Stream({ stream: writeable })
 }
 
-/**
- * Returns a valid Winston transport that writes to the passed array object
- * @param array Array to be used to store the "written" chunks
- * @returns {winston.transports.Stream}
- */
-function inMemory(array, options = {}) {
-  const memoryStream = new Writable({
-    objectMode: true,
-    write: (chunk, encoding, next) => {
-      array.push(chunk);
-      next()
-    }
-  });
-  return new winston.transports.Stream({stream: memoryStream, ...options})
-}
-
 module.exports = {
-  createMockTransport,
-  inMemory
+  createMockTransport
 };
