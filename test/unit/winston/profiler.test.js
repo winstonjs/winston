23ac781acd549730b430821c6ac252e7b9950fb9
Debadutta Panda <debadebaduttapanda.7@gmail.com>
2022-10-29 13:33:16 +0530

    change profiler testcase and force profiler to avoid object that is not a instance of logger

--- test/unit/winston/profiler.test.js
+++ test/unit/winston/profiler.test.js
@@ -7,27 +7,26 @@
  */
 
 const assume = require('assume');
+const Logger = require('../../../lib/winston/logger');
 const Profiler = require('../../../lib/winston/profiler');
-
 describe('Profiler', function () {
   it('new Profiler()', function () {
     assume(function () {
-      var profiler = new Profiler();
+      new Profiler();
     }).throws();
   });
 
   it('.done({ info })', function (done) {
-    var profiler = new Profiler({
-      write: function (info) {
-        assume(info).is.an('object');
-        assume(info.something).equals('ok');
-        assume(info.level).equals('info');
-        assume(info.durationMs).is.a('number');
-        assume(info.message).equals('testing1');
-        done();
-      }
-    });
-
+    const logger = new Logger();
+    logger.write = function (info) {
+      assume(info).is.an('object');
+      assume(info.something).equals('ok');
+      assume(info.level).equals('info');
+      assume(info.durationMs).is.a('number');
+      assume(info.message).equals('testing1');
+      done();
+    };
+    var profiler = new Profiler(logger);
     setTimeout(function () {
       profiler.done({
         something: 'ok',
