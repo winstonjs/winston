f60d4a5b8b0964757c88f5789276d7cce46d99b8
Debadutta Panda <debadebaduttapanda.7@gmail.com>
2023-06-24 22:42:05 +0530

    add additional testcase

--- test/unit/winston/profiler.test.js
+++ test/unit/winston/profiler.test.js
@@ -9,11 +9,12 @@
 const assume = require('assume');
 const Logger = require('../../../lib/winston/logger');
 const Profiler = require('../../../lib/winston/profiler');
+const { PassThrough } = require('stream');
 describe('Profiler', function () {
   it('new Profiler()', function () {
     assume(function () {
       new Profiler();
-    }).throws();
+    }).throws('Logger is required for profiling');
   });
 
   it('.done({ info })', function (done) {
@@ -35,4 +36,30 @@ describe('Profiler', function () {
       });
     }, 200);
   });
+
+  it('non logger object', function(){
+    assume(function() {
+      new Profiler(new Error('Unknown error'));
+    }).throws('Logger is required for profiling');
+
+    assume(function () {
+      new Profiler({a:'b'});
+    }).throws('Logger is required for profiling');
+
+    assume(function(){
+      new Profiler([1,2,3,4]);
+    }).throws('Logger is required for profiling');
+
+    assume(function () {
+      new Profiler(new PassThrough());
+    }).throws('Logger is required for profiling');
+
+    assume(function () {
+      new Profiler(2);
+    }).throws('Logger is required for profiling');
+    
+    assume(function () {
+      new Profiler('1');
+    }).throws('Logger is required for profiling');
+  })
 });
