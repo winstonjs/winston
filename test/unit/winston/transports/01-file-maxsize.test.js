f7e7f2f9ccca06e5edba17c03da9ed7c97b48d91
Pulkit Aggarwal <82268257+Pulkit0729@users.noreply.github.com>
2023-07-10 11:44:31 +0530

    Added Lazy option to file transport (#2317)
    * Test for file Transport option rotationFormat
    
    * Test for file Transport option rotationFormat
    
    * Added Lazy option in file transport
    
    * Lint and test
    
    * removed only statement
    
    * Update test/unit/winston/transports/01-file-maxsize.test.js
    
    Co-authored-by: David Hyde <DABH@users.noreply.github.com>
    
    * Update test/unit/winston/transports/01-file-maxsize.test.js
    
    Co-authored-by: David Hyde <DABH@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: myAlapi <myalapi2022@gmail.com>
    Co-authored-by: David Hyde <DABH@users.noreply.github.com>

--- test/unit/winston/transports/01-file-maxsize.test.js
+++ test/unit/winston/transports/01-file-maxsize.test.js
@@ -25,8 +25,8 @@ describe('File (maxsize)', function () {
   this.timeout(10000);
 
   let testDone = false;
-  before(removeFixtures);
-  after(done => {
+  this.beforeEach(removeFixtures);
+  this.afterEach(done => {
     testDone = true;
     removeFixtures(done);
   });
@@ -117,6 +117,96 @@ describe('File (maxsize)', function () {
 
       files.push(file);
       setImmediate(() => logKbytes(4));
+    });
+  });
+
+  describe('With lazy option enabled', () => {
+    it('should not create extra file', function (done) {
+      const fillWith = ['a', 'b', 'c', 'd', 'e'];
+      const lazyTransport = new winston.transports.File({
+        format: winston.format.printf(info => info.message),
+        filename: path.join(testLogFixturesPath, 'testmaxsize.log'),
+        maxsize: 3072,
+        lazy: true
+      });
+      const logger = winston.createLogger({
+        transports: [lazyTransport]
+      });
+      //
+      // Setup a list of files which we will later stat.
+      //
+      const files = [];
+
+      //
+      // Assets the no of files and all the files have been created with the
+      // correct filesize 
+      //
+      function assumeFilesCreated() {
+        assume(files.length).equals(fillWith.length);
+        files.map(function (file, i) {
+          let stats;
+          try {
+            stats = fs.statSync(file);
+          } catch (ex) {
+            assume(stats).is.an(
+              'object',
+              `${file} failed to open: ${ex.message}`
+            );
+          }
+
+          const text = fs.readFileSync(file, 'utf8');
+          assume(text[0]).equals(fillWith[i]);
+          // Either 4096 on Unix or 4100 on Windows
+          // because of the eol.
+          if (process.platform === 'win32') {
+            assume(stats.size).equals(3075);
+          } else {
+            assume(stats.size).equals(3072);
+          }
+        });
+        done();
+      }
+
+      //
+      // Log the specified kbytes to the transport
+      //
+      function logKbytes(kbytes) {
+        //
+        // Shift the next fill char off the array then push it back
+        // to rotate the chars.
+        //
+        const filler = fillWith.shift();
+        fillWith.push(filler);
+
+        //
+        //
+        // To not make each file not fail the assertion of the filesize we can
+        // make the array 1023 characters long.
+        //
+        const kbStr = Array(1023).fill(filler).join('');
+        for (var i = 0; i < kbytes; i++) {
+          logger.log({ level: 'info', message: kbStr });
+        }
+      }
+
+      // Initial Log
+      let count =1;
+      logKbytes(3);
+
+      // Listen to file close event called when the file is closed
+      lazyTransport.on('fileclosed', ()=>{
+        if (count === fillWith.length) {
+          assumeFilesCreated();
+          return;
+        }
+        count += 1;
+        setImmediate(()=>{logKbytes(3);});
+      })
+
+      //Listent to file open event called when the file is opened
+      lazyTransport.on('open', file => {
+        files.push(file);
+      });
     });
   });
 });
