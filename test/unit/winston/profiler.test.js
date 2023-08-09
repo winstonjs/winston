914b846846c5970711b5cba609dfbeb42c8580a7
Debadutta Panda <debadebaduttapanda.7@gmail.com>
2023-08-09 10:53:14 +0530

    feat: add guardrails to the instantiation of a Profiler (#2226)
    Authored-by: debaduttakore <139857436+debaduttakore@users.noreply.github.com>
    Reviewed-by: @wbt @maverick1872 
    Merged-by: maverick1872 <10217028+maverick1872@users.noreply.github.com>
    Ref: 2091

--- test/unit/winston/profiler.test.js
+++ test/unit/winston/profiler.test.js
@@ -7,27 +7,27 @@
  */
 
 const assume = require('assume');
+const Logger = require('../../../lib/winston/logger');
 const Profiler = require('../../../lib/winston/profiler');
-
+const { PassThrough } = require('stream');
 describe('Profiler', function () {
   it('new Profiler()', function () {
     assume(function () {
-      var profiler = new Profiler();
-    }).throws();
+      new Profiler();
+    }).throws('Logger is required for profiling');
   });
 
   it('.done({ info })', function (done) {
-    var profiler = new Profiler({
-      write: function (info) {
-        assume(info).is.an('object');
-        assume(info.something).equals('ok');
-        assume(info.level).equals('info');
-        assume(info.durationMs).is.a('number');
-        assume(info.message).equals('testing1');
-        done();
-      }
-    });
-
+    const logger = new Logger();
+    logger.write = function (info) {
+      assume(info).is.an('object');
+      assume(info.something).equals('ok');
+      assume(info.level).equals('info');
+      assume(info.durationMs).is.a('number');
+      assume(info.message).equals('testing1');
+      done();
+    };
+    var profiler = new Profiler(logger);
     setTimeout(function () {
       profiler.done({
         something: 'ok',
@@ -36,4 +36,30 @@ describe('Profiler', function () {
       });
     }, 200);
   });
+
+  it('non logger object', function(){
+    assume(function() {
+      new Profiler(new Error('Unknown error'));
+    }).throws('Logger is required for profiling');
+
+    assume(function () {
+      new Profiler({a:'b'});
+    }).throws('Logger is required for profiling');
+
+    assume(function(){
+      new Profiler([1,2,3,4]);
+    }).throws('Logger is required for profiling');
+
+    assume(function () {
+      new Profiler(new PassThrough());
+    }).throws('Logger is required for profiling');
+
+    assume(function () {
+      new Profiler(2);
+    }).throws('Logger is required for profiling');
+    
+    assume(function () {
+      new Profiler('1');
+    }).throws('Logger is required for profiling');
+  })
 });
