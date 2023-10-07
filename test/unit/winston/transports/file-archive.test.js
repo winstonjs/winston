58ef11b190f463b051e8abd39f6781f34f56330b
Jack Nolddor <1280022+nolddor@users.noreply.github.com>
2023-10-07 09:28:09 +0200

    fix: rimraf is not a function

--- test/unit/winston/transports/file-archive.test.js
+++ test/unit/winston/transports/file-archive.test.js
@@ -8,7 +8,7 @@
 
 /* eslint-disable no-sync */
 const assert = require('assert');
-const rimraf = require('rimraf');
+const { rimraf } = require('rimraf');
 const fs = require('fs');
 const path = require('path');
 const winston = require('../../../../lib/winston');
