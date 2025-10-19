b7735d8ebd524b02ec039af5dd9c0c274554d7c8
abdullah <shahzadabdullah814@gmail.com>
2025-10-19 01:13:33 -0700

    fixed bug when custom levels have on key/value pair it will take first as default level value

--- test/unit/winston/implicit-level.test.js
+++ test/unit/winston/implicit-level.test.js
@@ -1,4 +1,5 @@
-const {createLogger} = require("../../../lib/winston");
+const { levels } = require("logform");
+const {createLogger, level} = require("../../../lib/winston");
 
 describe('createLogger() with custom levels and level should be calculated implicitly', () => {
 
@@ -19,6 +20,14 @@ describe('createLogger() with custom levels and level should be calculated impli
     // logger.level should be the median (medium) / calculated implicitly
     expect(logger.level).toBe('fourth');
   });
+  test('Should default to second level if level  specified', () => {
+    const customLevels = { first: 0, second: 1, third: 2 , fourth :4,fifth:5 ,sixth:6 , seventh:7,eight:8};
+    
+    const logger = createLogger({levels: customLevels,level:"fourth"});
+
+    // logger.level should be the median (medium) / calculated implicitly
+    expect(logger.level).toBe('fourth');
+  });
 
   test('should default to info level if level not specified and levels not specified', () => {
     
@@ -26,6 +35,26 @@ describe('createLogger() with custom levels and level should be calculated impli
 
     // logger.level should be the median (medium) / calculated implicitly
     expect(logger.level).toBe('info');
+  });
+
+  test('should default to firstLevel when   levels  specified', () => {
+    
+    const logger = createLogger({levels:{
+      firstLevel:0
+    }});
+
+    // logger.level should be the median (medium) / calculated implicitly
+    expect(logger.level).toBe('firstLevel');
+  });
+  test('should default to firstLevel when   levels  specified', () => {
+    
+    const logger = createLogger({levels:{
+      firstLevel:0,
+      secondLevel:1,
+    }});
+
+    // logger.level should be the median (medium) / calculated implicitly
+    expect(logger.level).toBe('firstLevel');
   });
 
 });
